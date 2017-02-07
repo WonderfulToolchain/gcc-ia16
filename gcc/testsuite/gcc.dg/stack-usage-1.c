b126b07c76773a642bd6f750f662406b54945e68
Andrew Jenner <andrew@reenigne.org>
2017-02-07 08:41:32 +0000

    Stack usage and sibcalls working

--- gcc/testsuite/gcc.dg/stack-usage-1.c
+++ gcc/testsuite/gcc.dg/stack-usage-1.c
@@ -107,6 +107,8 @@
 #  define SIZE 252
 #elif defined (__loongarch_lp64)
 #  define SIZE 240   /* 256 - 8 bytes for $fp, and 8 bytes for a temp value */
+#elif defined (__ia16__)
+#  define SIZE 252
 #else
 #  define SIZE 256
 #endif
